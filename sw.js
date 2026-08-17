33e447cd14d7353939b0340396c65acf7ba1d225
R.VASANTH KUMAR <125186174+Vasanth027@users.noreply.github.com>
2026-08-17 14:02:08 +0530

    feat: add service worker for offline shell

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'rainguard-v1';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (new URL(request.url).origin === self.location.origin) {
+    event.respondWith(caches.match(request).then((cached) => cached || fetch(request)));
+  }
+});
